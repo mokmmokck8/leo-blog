9b82df78ac77ddeb49f28c0f6d1e851bbc6fa547
Leo Mok <43039231+mokmmokck8@users.noreply.github.com>
2023-12-23 14:05:49 +0800

    feat: add tailwind and react and shadcn

---
+++ src/components/MyFirstComponent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "./ui/button";
+
+export const MyFirstComponent = () => {
+  return (
+    <div>
+      <Button>Hello World</Button>
+    </div>
+  );
+};
